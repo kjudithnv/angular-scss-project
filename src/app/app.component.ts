97936738466f6bbbea409106d52cb968023f1f28
Judith Vera <jud_1016@hotmail.com>
2021-07-11 12:43:10 -0700

    new project

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './models/Todo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'To Do List';
+
+  toDos:Todo[];
+inputTodo:string = "";
+
+    constructor() { }
+    ngOnInit(): void {
+  
+      this.toDos = [
+        {
+          content: 'Get Bread',
+          completed: false
+        },
+        {
+          content: 'Stream',
+          completed: true
+        }
+      ]
+
+  }
+
+    toggleDone (id) {
+        this.toDos.map((v,i) => {
+          if (i == id) v.completed = !v.completed;
+
+          return v;
+        })
+    }
+
+    deleteTodo (id) {
+      this.toDos = this.toDos.filter((v,i)  => i !== id)
+    }
+
+    addTodo () {
+      this.toDos.push({
+        content: this.inputTodo,
+        completed: false
+      });
+
+        this.inputTodo = "";
+    }
+  }
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
